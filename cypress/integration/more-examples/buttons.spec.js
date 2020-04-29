40b8c37c2ab6f9efee0250d72f8dc7fe1c370c31
CJ Joulain <cj.joulain@tuftandneedle.com>
2020-04-29 16:10:02 -0700

    Update selector for the first homepage button.

--- cypress/integration/more-examples/buttons.spec.js
+++ cypress/integration/more-examples/buttons.spec.js
@@ -5,10 +5,12 @@ describe('Click all buttons on the TN prod homepage', () => {
   });
 
 // let's refactor this
-  it('clicks on the first button', () => {
-    cy.get('#cms-home-hero-variant-4 > div.content > div.hero__controls > a').contains('Shop now');
-    cy.get('#cms-home-hero-variant-4 > div.content > div.hero__controls > a').should('be.visible');
-    cy.get('#cms-home-hero-variant-4 > div.content > div.hero__controls > a').click();
+  it.only('clicks on the first button', () => {
+    cy.get('.hero__cta').contains('Shop now');
+    cy.get('.hero__cta').should('be.visible');
+    cy.get('#error_page').should('not.be.visible');
+    cy.get('.hero__cta').click();
+    cy.url().should('include', `products/`)
   })
 
   it('clicks on the second button', () => {
@@ -17,3 +19,4 @@ describe('Click all buttons on the TN prod homepage', () => {
 
 });
 
+// scrollIntoView() -> non-desktop devices
